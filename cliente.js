eb7412e954d97494564f33c01bb3849e3e4c0645
geovanna lima <lsgeovanna072@gmail.com>
2022-05-30 16:10:28 -0300

    correção das funções

--- cliente.js
+++ cliente.js
@@ -1,31 +1,37 @@
-function validarNome() {
-    var nome = ""
+function validarNome(nome) {
     if (nome === "" ) {
-        return console.log("O nome não foi preenchido")
+        return "O nome não foi preenchido"
+    }
+    if(nome.length < 3) {
+        return "O nome deve conter 3 caracteres ou mais"
+    }
+} 
+
+function validarSobrenome(sobrenome) {
+    if (sobrenome === "" ) {
+        return "O sobrenome não foi preenchido"
     }
 }
 
-function validarSobrenome() {
-    var sobrenome = ""
-    if (sobrenome ==="" ) {
-        return console.log("O sobrenome não foi preenchido")
+function validarCep(cep) {
+    if (cep === "" ) {
+        return "O cep não foi preenchido"
+    }
+    if(cep.length >= 10) {
+        return "O cep deve conter apenas 9 caracteres"
+    }
+} 
+
+function validarEmail(email){
+    let arroba = email.indexOf("@")
+    let ponto = email.indexOf(".")
+    if(arroba = -1){
+        return console.log("o email nao contem o caracter @")
+    }
+    if(ponto = -1){
+        return console.log("o email nao contem o caracter .")
     }
 }
 
-function validarEmail(){
-    var email = ""
-    if(email = ('[a-z0-9]+@[A-Z]+.')){
-        return true
-    }else {
-        return console.log("email invalido")
-    }
-}
 
-function validarCep(){
-    var cep = ""
-    if(cep > 9){
-        return console.log("o cep deve conter apenas 9 digitos")
-    }
-}
-
-const Estados = [Rondônia - RD, Acre - AC, Amazonas - AM, Roraima - RR, Pará - PA, Amapá - AP, Tocantins - TO, Maranhão - MA, Piauí - PI, Ceará - CE, RioGrandeDoNorte - RN, Paraíba - PB, Pernambuco - PE, Alagoas - AL, Sergipe - SE, Bahia - BA, MinasGerais - MG, EspíritoSanto - ES, RioDeJaneiro - RJ, SãoPaulo - SP, Paraná - PR, SantaCatarina - SC, RioGrandeDoSul - RS, MatoGrossoDoSul - MS, MatoGrosso - MT, Goiás - GO, DistritoFederal - DF]
+const Estados = ["Rondônia - RD", "Acre - AC", "Amazonas - AM", "Roraima - RR", "Pará - PA", "Amapá - AP", "Tocantins - TO", "Maranhão - MA", "Piauí - PI", "Ceará - CE", "Rio Grande Do Norte - RN", "Paraíba - PB", "Pernambuco - PE", "Alagoas - AL", "Sergipe - SE", "Bahia - BA", "Minas Gerais - MG", "Espírito Santo - ES", "Rio De Janeiro - RJ", "São Paulo - SP", "Paraná - PR", "Santa Catarina - SC", "Rio Grande Do Sul - RS", "Mato Grosso Do Sul - MS", "Mato Grosso - MT", "Goiás - GO", "Distrito Federal - DF"]
