f0b06c2d40438a96f4923530095b71a49d60df62
geovanna lima <lsgeovanna072@gmail.com>
2022-06-01 11:50:11 -0300

    corrigi as funções e adicionei listas

--- cliente.js
+++ cliente.js
@@ -17,21 +17,35 @@ function validarCep(cep) {
     if (cep === "" ) {
         return "O cep não foi preenchido"
     }
-    if(cep.length >= 10) {
-        return "O cep deve conter apenas 9 caracteres"
+    if(cep.length >= 9) {
+        return "O cep deve conter apenas 8 caracteres"
     }
 } 
 
 function validarEmail(email){
     let arroba = email.indexOf("@")
     let ponto = email.indexOf(".")
-    if(arroba = -1){
-        return console.log("o email nao contem o caracter @")
+    if(arroba == -1){
+        return "o email nao contem o caracter @"
     }
-    if(ponto = -1){
-        return console.log("o email nao contem o caracter .")
+    if(ponto == -1){
+        return "o email nao contem o caracter ."
     }
 }
 
+function listarEstados(){
+    var estados = ["Rondônia - RD", "Acre - AC", "Amazonas - AM", "Roraima - RR", "Pará - PA", "Amapá - AP", "Tocantins - TO", "Maranhão - MA", "Piauí - PI", "Ceará - CE", "Rio Grande Do Norte - RN", "Paraíba - PB", "Pernambuco - PE", "Alagoas - AL", "Sergipe - SE", "Bahia - BA", "Minas Gerais - MG", "Espírito Santo - ES", "Rio De Janeiro - RJ", "São Paulo - SP", "Paraná - PR", "Santa Catarina - SC", "Rio Grande Do Sul - RS", "Mato Grosso Do Sul - MS", "Mato Grosso - MT", "Goiás - GO", "Distrito Federal - DF"]
+    return estados
+}
 
-const Estados = ["Rondônia - RD", "Acre - AC", "Amazonas - AM", "Roraima - RR", "Pará - PA", "Amapá - AP", "Tocantins - TO", "Maranhão - MA", "Piauí - PI", "Ceará - CE", "Rio Grande Do Norte - RN", "Paraíba - PB", "Pernambuco - PE", "Alagoas - AL", "Sergipe - SE", "Bahia - BA", "Minas Gerais - MG", "Espírito Santo - ES", "Rio De Janeiro - RJ", "São Paulo - SP", "Paraná - PR", "Santa Catarina - SC", "Rio Grande Do Sul - RS", "Mato Grosso Do Sul - MS", "Mato Grosso - MT", "Goiás - GO", "Distrito Federal - DF"]
+function listarEstadoCivil(){
+    var estadoCivil = ["Solteiro", "Casado", "Separado", "Divorciado", "Viuvo"]
+    return estadoCivil
+}
+
+function listarGenero(){
+    var generos = ["Masculino", "Feminino", "Não binario"]
+    return generos
+}
+
+
